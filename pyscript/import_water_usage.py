adf7fa97783d993e0dc7a8bbb83d1d5487e8fa7b
BJReplay <37993507+BJReplay@users.noreply.github.com>
2026-02-18 16:22:59 +1100

    Update import_water_usage to handle additional parameters
    Refactor water usage import functions to include new parameters and improve date handling.

--- pyscript/import_water_usage.py
+++ pyscript/import_water_usage.py
@@ -1,29 +1,28 @@
-from datetime import datetime  # noqa: D100, INP001
+from datetime import datetime, timedelta, date  # noqa: D100, INP001
 import json
 import os
 from pathlib import Path
-
+import logging
 import requests
 
-MAINS_WATER_SERIAL = "mains_water_serial"
-RECYCLED_WATER_SERIAL = "recycled_water_serial"
 SEW_USERNAME = "sew_username"
 SEW_PASSWORD = "sew_password"
+SEW_BAID = "sew_baid"
+SEW_METERID = "sew_meterid"
 TARGET_DATE = "target_date"
 CODE = "code"
 CONTEXT = "context"
 GET_RECYCLED = False
 
+_LOGGER = logging.getLogger(__name__)
+
 @service  # noqa: F821
 def import_yesterdays_water_usage(
     mains_water_stat_id,
-    mains_water_serial,
     sew_username,
     sew_password,
     browserless: str,
     token: str = "",
-    recycled_water_stat_id: str = "",
-    recycled_water_serial: str = "",
 ):
     """Get Water Usage for Yesterday.
 
@@ -33,39 +32,34 @@ def import_yesterdays_water_usage(
 
     Arguments:
         mains_water_stat_id: The sensor to return the mains water usage to. Example: sensor.water_usage_mains
-        mains_water_serial: The serial number of the mains water meter. Example: SAHA000000
         sew_username: Your Username for the South East Water (SEW) website. Example: myusername@gmail.com
         sew_password: Your Password for the South East Water (SEW) website. Example: myC0mpl3xP@55w0rd
         browserless: The URL that the browserless instance is running on. Example: http://localhost:3000
 
     Keyword Arguments:
-        recycled_water_stat_id: The sensor to return the mains water usage to. Example: sensor.water_usage_recycled (default: {""})
-        recycled_water_serial: The serial number of the mains water meter. Example: RAHA000000 (default: {""})
-        token: The browserless token to use. Example: 6R0W53R135510, or blank if running on the HASS addon
+        token: The browserless token to use. Example: 6R0W53R135510, or BLANK if running on the HASS addon
 
     """
+    yesterday = date.today() - timedelta(days = 1)
+
     import_water_usage(
         mains_water_stat_id=mains_water_stat_id,
-        mains_water_serial=mains_water_serial,
         sew_username=sew_username,
         sew_password=sew_password,
-        target_date="",
+        target_date=yesterday,
         browserless=browserless,
-        token=token,
-        recycled_water_stat_id=recycled_water_stat_id,
-        recycled_water_serial=recycled_water_serial,    )
+        token=token)
 
 @service  # noqa: F821
 def import_water_usage(
     mains_water_stat_id,
-    mains_water_serial,
     sew_username,
     sew_password,
     target_date: datetime,
     browserless: str,
     token: str = "",
-    recycled_water_stat_id: str = "",
-    recycled_water_serial: str = "",
+    default_sew_baId: str = "",
+    default_sew_meterId: str = "",
 ):
     """Get Water Usage for Date.
 
@@ -75,16 +69,15 @@ def import_water_usage(
 
     Arguments:
         mains_water_stat_id: The sensor to return the mains water usage to. Example: sensor.water_usage_mains
-        mains_water_serial: The serial number of the mains water meter. Example: SAHA000000
         sew_username: Your Username for the South East Water (SEW) website. Example: myusername@gmail.com
         sew_password: Your Password for the South East Water (SEW) website. Example: myC0mpl3xP@55w0rd
         target_date: The date you want to import data for. Example: 2024-11-27
         browserless: The URL that the browserless instance is running on. Example: http://localhost:3000
 
     Keyword Arguments:
-        recycled_water_stat_id: The sensor to return the mains water usage to. Example: sensor.water_usage_recycled (default: {""})
-        recycled_water_serial: The serial number of the mains water meter. Example: RAHA000000 (default: {""})
         token: The browserless token to use. Example: 6R0W53R135510, or blank if running on the HASS addon
+        default_sew_baId: The default SEW internal Account ID (baId) to pass in. Retrieve from Local Storage using developer tools. Example: b02341111112b5EITGY
+        default_sew_meterId: The default SEW internal Meter ID (meterId) to pass in. Retrieve from Local Storage using developer tools. Example: c2E82222222ZG1FEBT
 
     """
 
@@ -95,33 +88,55 @@ def import_water_usage(
     js_executable = js_exec_str.decode()
     os.close(fd)
 
-    context = {
-        MAINS_WATER_SERIAL: mains_water_serial,
-        RECYCLED_WATER_SERIAL: recycled_water_serial,
-        SEW_USERNAME: sew_username,
-        SEW_PASSWORD: sew_password,
-        TARGET_DATE: target_date,
-        GET_RECYCLED: False,
-    }
+    if not target_date or target_date == "":
+        target_date = datetime.now() - timedelta(days=1)
+    initial_date: datetime = datetime.strptime(target_date, "%Y-%m-%d")
+    current_date: datetime = datetime.strptime(target_date, "%Y-%m-%d")
+    current_date_str: str = current_date.strftime("%Y-%m-%d")
+    retrieved_date: datetime = current_date - timedelta(days = 1)
 
-    headers = {"Content-Type": "application/json"}
-    data = json.dumps({CODE: js_executable, CONTEXT: context})
-    if token == "" or token is None:
-        url = f"{browserless}/function"
-    else:
-        url = f"{browserless}/function?token={token}"
+    while retrieved_date < current_date:
 
-    usage_response = task.executor(  # noqa: F821
-        requests.request,
-        method="POST",
-        url=url,
-        headers=headers,
-        data=data,
-    )
-    usage_response_data = json.loads(usage_response.text)
+        context = {
+            SEW_USERNAME: sew_username,
+            SEW_PASSWORD: sew_password,
+            TARGET_DATE: current_date_str,
+            GET_RECYCLED: False,
+            SEW_BAID: default_sew_baId,
+            SEW_METERID: default_sew_meterId
+        }
 
-    # Import the data to statistics
-    import_water_usage_data(mains_water_stat_id, "mains", usage_response_data)
+        headers = {"Content-Type": "application/json"}
+        data = json.dumps({CODE: js_executable, CONTEXT: context})
+        if token == "" or token is None:
+            url = f"{browserless}/function"
+        else:
+            url = f"{browserless}/function?token={token}"
+
+        usage_response = task.executor(  # noqa: F821
+            requests.request,
+            method="POST",
+            url=url,
+            headers=headers,
+            data=data,
+        )
+
+        usage_response_data = json.loads(usage_response.text)
+        retrieved_date: datetime = datetime.strptime(usage_response_data["mains"]["apiDate"].replace("T00:00:00+00:00", ""), "%Y-%m-%d")
+
+        if retrieved_date >= initial_date:
+            # Import the data to statistics
+            import_water_usage_data(mains_water_stat_id, "mains", usage_response_data)
+        else:
+            # bump forward a day to account for SEW retrieving a day prior to request, and loop
+            current_date = current_date + timedelta(days = 1)
+            current_date_str: str = current_date.strftime("%Y-%m-%d")
+        
+        if not target_date or target_date == "":
+            break
+        
+        if retrieved_date >= initial_date:
+            break
 
 @service  # noqa: F821
 def import_file_water_usage(stat_id, file_path_under_config):
@@ -162,21 +177,21 @@ def import_water_usage_data(stat_id, type, data):
     """
     starting_point = float(state.get(stat_id))  # noqa: F821
 
-    readings = data[type]["Readings"]
+    readings = data[type]["readings"]
 
     tally = starting_point
 
-    for reading in readings:
-        litres = reading["Measurement"]
+    for idx, reading in enumerate(readings):
+        litres = reading
 
         if litres is None:
             continue  # skip value if no usage
 
-        data_date = reading["Date"].replace(".000Z", "").replace("T", " ")
-        datetime_object = datetime.strptime(data_date, "%Y-%m-%d %H:%M:%S")
+        data_date = data[type]["apiDate"].replace("+00:00", "").replace("T", " ")
+        datetime_object = datetime.strptime(data_date, "%Y-%m-%d %H:%M:%S") + timedelta(hours=idx)
 
         start = str(datetime_object.astimezone())
-        tally += litres
+        tally += litres #TODO
 
         statistic_element = {"start": start, "state": tally, "sum": tally, "max": tally}
 
